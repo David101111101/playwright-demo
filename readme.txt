61ad130a1eb451b6305c74273204c7187b0727ea
Dave <davidstevenabril@gmail.com>
2025-01-21 19:42:51 -0500

    API Debugging with Playwright tracing

---
+++ readme.txt
@@ -0,0 +1,13 @@
+Ecommerce end to end Test FIle:
+ecommercePurchase.spec.ts
+
+
+API:
+npm run test:api
+
+
+Debug:
+npm run test:debug
+
+To activate tracing tests:
+npx playwright test ecommercePurchase --trace on
--- tests/assert.spec.ts
+++ tests/assert.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test('pLaying with assertions', async ({ page }) => {
-  await page.goto('https://uitestingplayground.com/textinput');
-    await expect(page.locator('#newButtonName')).toBeVisible();
+  await page.goto('https://www.duckduckgo.com');
+    await expect(page.locator('#searchbox_input')).toBeVisible();
 
-  await page.locator('#newButtonName').fill("Hello World");
+  await page.locator('#searchbox_input').fill("Hello World");
 
-  await page.locator('updatingButton').click();
-  await expect(page.locator('#updatingButton')).toContainText('Hello World');
+  await page.locator('#searchbox_homepage > div > div.searchbox_searchbox__eaWKL > div > button.iconButton_button__6x_9C.searchbox_searchButton__F5Bwq').click();
+  
 
 
 });
